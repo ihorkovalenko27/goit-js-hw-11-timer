963c50be116849eeec4177d88b6edb426bc3606c
Ihor Kovalenko <kovalyan27@gmail.com>
2021-07-10 00:55:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 
 const daysValue = document.querySelector('span[data-value="days"]');
+
 const hoursValue = document.querySelector('span[data-value="hours"]');
+
 const minsValue = document.querySelector('span[data-value="mins"]');
+
 const secsValue = document.querySelector('span[data-value="secs"]');
 
 class CountdownTimer {
@@ -18,11 +21,13 @@ class CountdownTimer {
         }, 1000);
     }
 
+
     getDeltaTime() {
         const currentTime = Date.now();
         const deltaTime = this.targetDate - currentTime;
         this.getTimeComponents(deltaTime);
     }
+
 
     getTimeComponents(time) {
         const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -34,6 +39,7 @@ class CountdownTimer {
 
         this.updateClockface(days, hours, mins, secs);
     }
+
 
     pad(value) {
         return String(value).padStart(2, '0');
@@ -48,6 +54,7 @@ class CountdownTimer {
     }
 
 }
+
 
 
 const timer = new CountdownTimer({
